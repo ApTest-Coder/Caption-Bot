7b91b53d41c3a6eb407493c9c60d591e2ec23b9e
ApTest-Coder <bsailen59@gmail.com>
2026-08-17 00:48:32 +0530

    style: format logger helper

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,11 @@
+"""Logging configuration."""
+
 import logging
 
-def setup():
-    logging.basicConfig(level=logging.INFO,format='%(asctime)s | %(levelname)s | %(name)s | %(message)s')
+
+def setup() -> None:
+    """Configure application-wide console logging."""
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    )

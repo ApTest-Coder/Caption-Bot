e6ffa4be2fc5cec4f19b783842ada5b934b70adc
ApTest-Coder <bsailen59@gmail.com>
2026-08-17 00:49:04 +0530

    style: format user database helper

--- database/users.py
+++ database/users.py
@@ -1,5 +1,8 @@
 """User database helpers."""
+
 from .settings import Database
 
+
 async def is_admin(db: Database, user_id: int) -> bool:
+    """Return whether the user is an administrator."""
     return await db.is_admin(user_id)

be821d120bcdc5f68b0800a703dc0919cbee197a
ApTest-Coder <bsailen59@gmail.com>
2026-08-17 00:48:23 +0530

    style: format floodwait helper

--- utils/floodwait.py
+++ utils/floodwait.py
@@ -1,4 +1,8 @@
+"""Flood-wait sleep helper."""
+
 import asyncio
 
-async def sleep_for(seconds):
-    await asyncio.sleep(max(0,int(seconds)))
+
+async def sleep_for(seconds: int | float) -> None:
+    """Sleep for a non-negative number of seconds."""
+    await asyncio.sleep(max(0, int(seconds)))

a453205ff672365a63a88b43aaf13aba066759a7
ApTest-Coder <bsailen59@gmail.com>
2026-08-16 15:40:45 +0530

    Fix SQLite schema

--- database/sqlite.py
+++ database/sqlite.py
@@ -3,6 +3,11 @@ from config import SQLITE_DATABASE
 
 async def connect():
     db=await aiosqlite.connect(SQLITE_DATABASE)
-    await db.executescript('CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY,username TEXT,blocked INTEGER DEFAULT 0,first_seen TEXT,last_seen TEXT); CREATE TABLE IF NOT EXISTS channels(channel_id INTEGER PRIMARY KEY,owner_id INTEGER,title TEXT,username TEXT,config TEXT); CREATE TABLE IF NOT EXISTS admins(user_id INTEGER PRIMARY KEY;')
+    await db.executescript('''
+    CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY,username TEXT,blocked INTEGER DEFAULT 0,first_seen TEXT,last_seen TEXT);
+    CREATE TABLE IF NOT EXISTS channels(channel_id INTEGER PRIMARY KEY,owner_id INTEGER,title TEXT,username TEXT,config TEXT);
+    CREATE TABLE IF NOT EXISTS admins(user_id INTEGER PRIMARY KEY);
+    CREATE TABLE IF NOT EXISTS stats(key TEXT PRIMARY KEY,value INTEGER DEFAULT 0);
+    ''')
     await db.commit()
     return db

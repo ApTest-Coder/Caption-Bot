e673642fe9ad3484cb181c0c94254ad69b3f3b9e
ApTest-Coder <bsailen59@gmail.com>
2026-08-17 00:48:19 +0530

    style: format error logging helper

--- utils/errors.py
+++ utils/errors.py
@@ -1,5 +1,10 @@
-import logging
-log=logging.getLogger('caption-bot')
+"""Error logging helpers."""
 
-def log_exception(exc):
-    log.exception('Unhandled bot error: %s',exc)
+import logging
+
+LOGGER = logging.getLogger("caption-bot")
+
+
+def log_exception(exc: Exception) -> None:
+    """Log an exception with traceback information."""
+    LOGGER.exception("Unhandled bot error: %s", exc)

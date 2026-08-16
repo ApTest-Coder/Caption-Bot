7f4eb96e2cf8b9d919f399e9cae4696b78c4a4b2
ApTest-Coder <bsailen59@gmail.com>
2026-08-16 15:43:24 +0530

    Use settings database facade

--- database/channels.py
+++ database/channels.py
@@ -1,8 +1,5 @@
 """Channel persistence helpers."""
-from .db import Database
+from .settings import Database
 
-async def get(db: Database, channel_id: int):
-    return await db.get_channel(channel_id)
-
-async def list_for_owner(db: Database, owner_id: int):
-    return await db.list_channels(owner_id)
+async def get(db: Database, channel_id: int): return await db.get_channel(channel_id)
+async def list_for_owner(db: Database, owner_id: int): return await db.list_channels(owner_id)

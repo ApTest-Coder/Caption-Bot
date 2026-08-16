a743a573d838fbd706a61711a4ad4902cb5496d7
ApTest-Coder <bsailen59@gmail.com>
2026-08-16 15:40:53 +0530

    Add channel database helpers

---
+++ database/channels.py
@@ -0,0 +1,8 @@
+"""Channel persistence helpers."""
+from .db import Database
+
+async def get(db: Database, channel_id: int):
+    return await db.get_channel(channel_id)
+
+async def list_for_owner(db: Database, owner_id: int):
+    return await db.list_channels(owner_id)

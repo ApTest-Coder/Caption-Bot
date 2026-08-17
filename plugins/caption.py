00883a2285b3e39599b100222452f138bf3b54f1
ApTest-Coder <bsailen59@gmail.com>
2026-08-17 11:08:36 +0530

    fix: remove unused caption plugin import

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 import asyncio
 
 from aiogram import Router
-from aiogram.enums import ButtonStyle
 from aiogram.exceptions import TelegramRetryAfter
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
